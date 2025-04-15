d57ae9a19578168cd3ed71877d1e5250d47d76d1
jeedrb <jar3d.xd@gmail.com>
2025-04-15 18:06:03 -0400

    Initial commit, template lol

---
+++ src/client/java/jeed/jeedskins/JeedSkinsClient.java
@@ -0,0 +1,10 @@
+package jeed.jeedskins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JeedSkinsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}

c1de453c4e99065c7bf49d5cafcca3f2f0f86188
jeedrb <jar3d.xd@gmail.com>
2025-04-17 02:05:55 -0400

    broken but falling asleep and need to save

--- src/main/java/jeed/jeedskins/item/ModItems.java
+++ src/main/java/jeed/jeedskins/item/ModItems.java
@@ -4,13 +4,17 @@ import jeed.jeedskins.JeedSkins;
 import net.minecraft.component.type.ConsumableComponent;
 import net.minecraft.component.type.ConsumableComponents;
 import net.minecraft.component.type.FoodComponent;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
+//import net.minecraft.item.ToolMaterial;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
+
+import net.fabricmc.fabric.api.itemgroup.v1.*;
 
 import java.util.function.Function;
 
@@ -26,6 +30,17 @@ public class ModItems {
             .consumeSeconds(0.1f)
             .build();
 
+    public static final ToolMaterial PLASTIC_TOOL_MATERIAL = new ToolMaterial(
+            BlockTags.INCORRECT_FOR_IRON_TOOL,
+            64,
+            5.0f,
+            0.5f,
+            1,
+            PlasticArmorMaterial.REPAIRS_PLASTIC_ARMOR
+    );
+
+
+
 //    public static final Item EDGE_PIECE = register("edge_piece", Item::new, new Item.Settings());
     public static final Item EDGE_PIECE = register("edge_piece", Item::new, new Item.Settings()
         .maxCount(12)
@@ -33,6 +48,14 @@ public class ModItems {
         .food(EDGE_PIECE_FOOD_COMPONENT, EDGE_PIECE_CONSUMABLE_COMPONENT)
         );
 
+
+    public static final Item PLASTIC_CORE = register(
+            "plastic_core",
+            settings -> new SwordItem(PLASTIC_TOOL_MATERIAL, 100f, 0.1f, settings),
+            new Item.Settings()
+    );
+    ItemGroupEvents.
+            // https://docs.fabricmc.net/develop/items/custom-tools
 
 
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
